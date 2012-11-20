0cb07c9da9ccf80ae9b2295b26d8f32908e2f614
Ryhan Hassan <rthprog@gmail.com>
2012-11-20 11:50:42 -0500

    Fixed js bug

--- js/portkey.js
+++ js/portkey.js
@@ -49,10 +49,7 @@ function PrintRules() {
 var css = PrintRules();
 
 document.documentElement.innerHTML = html;
-document.body.innerHTML += "<script type='text/javascript'>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-29614316-3']);_gaq.push(['_trackPageview']);(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();</script>";
+document.body.innerHTML +=  "<script type='text/javascript'>var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-29614316-3']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>";
 document.head.innerHTML += "<style>" + css + "</style>";
 
 var content = document.documentElement.innerHTML;
