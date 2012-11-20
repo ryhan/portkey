334c17c4357a64f1f5906ab7c522421da1584d01
Ryhan Hassan <rthprog@gmail.com>
2012-11-20 08:55:15 -0500

    Working beta

---
+++ js/portkey.js
@@ -0,0 +1,61 @@
+function strip(s) {
+    var div = document.createElement('html');
+    div.innerHTML = s;
+
+    var scripts = div.getElementsByTagName('script');
+    var i = scripts.length;
+    while (i--) {
+      scripts[i].parentNode.removeChild(scripts[i]);
+    }
+
+    var stylesheets = div.getElementsByTagName('link');
+    var i = stylesheets.length;
+    while (i--) {
+    	stylesheets[i].parentNode.removeChild(stylesheets[i]);
+    }
+
+    var stylesheets = div.getElementsByTagName('style');
+    var i = stylesheets.length;
+    while (i--) {
+    	stylesheets[i].parentNode.removeChild(stylesheets[i]);
+    }
+
+    return div.innerHTML;
+ }
+
+var html = strip( document.documentElement.innerHTML);
+
+function PrintRules() {
+	var genString = '';
+
+	var sheets = document.styleSheets;
+	for( var j = 0; j< sheets.length; j++){
+		var rules = sheets[j].rules || sheets[j].cssRules;
+		if (rules == null){
+			genString += '</style><link rel="stylesheet" type="text/css" href="' + sheets[j].href + '"/><style>'
+		}else{
+			for(var x=0;x<rules.length;x++) {
+				genString += rules[x].cssText;
+	    	}
+		}
+	}
+    return genString;
+}
+
+var css = PrintRules();
+
+document.documentElement.innerHTML = html;
+document.head.innerHTML += "<style>" + css + "</style>";
+
+var content = document.documentElement.innerHTML;
+
+document.body.innerHTML += 
+    '<form id="portkeyPOST" enctype="application/x-www-form-urlencoded"'
+        + ' action="http://localhost:5000/api" method="post">'
+        + '<input type="hidden" name="content" value="' + encodeURIComponent(content) + '">' 
+        + '</form>';
+
+document.getElementById('portkeyPOST').submit();
+
+
+
--- web.js
+++ web.js
@@ -2,16 +2,43 @@ var fs = require('fs'),
 	redisUrl = require('redis-url'),
 	http = require('http'),
 	nowjs = require('now');
+
+var querystring = require('querystring');
+var utils = require('utils');
 	
 // Connect Database
 var redis = redisUrl.connect(process.env.REDISTOGO_URL);
 
+var content = '';
+
+
+var linkTable = {};
+
+var genString = function(){
+	var vowels = ['a','e','i', 'o','u']; 
+	var other=['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t']; 
+	var str = ''; 
+	for(var i =0; i< 5; i++){ 
+		str += other[Math.floor(Math.random()*other.length)]; 
+		str+= vowels[Math.floor(Math.random()*vowels.length)];
+	}
+
+	if (linkTable[genString] != undefined){
+		return genString();
+	}
+	return str;
+};
+
+
 // Create Server
 var server = http.createServer(function(req, response)
 {
+	// application routes
 	var route = {
 		'/': ['/html/index.html', 'text/html'],
+		'/template': ['/html/template.html', 'text/html'],
 		'/jquery.js': ['/js/jquery.js', 'text/javascript'],
+		'/portkey.js': ['/js/portkey.js', 'text/javascript'],
 		'/main.css': ['/css/main.css', 'text/css']
 	}[req.url];
 
@@ -24,8 +51,56 @@ var server = http.createServer(function(req, response)
 		});
 	}
 	else{
-		response.writeHead(404, {'Content-Type': 'text/plain'});
-    	response.end("Page Could Not Be Found"); 
+
+		if (req.url.substring(0,2) == '//'){
+			// special request handler
+			response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+			response.write('<!DOCTYPE html><html>');
+	      	response.write(decodeURIComponent(linkTable[req.url.substring(2,req.url.length)]));
+	      	response.write('</html>');
+	      
+	      	response.end();
+
+		}else if (req.url == '/api' && req.method == 'POST'){
+			console.log("[200] " + req.method + " to " + req.url);
+		    var fullBody = '';
+		    
+		    req.on('data', function(chunk) {
+		      // append the current chunk of data to the fullBody variable
+		      fullBody += chunk.toString();
+		    });
+		    
+		    req.on('end', function() {
+		    
+		      // request ended -> do something with the data
+		      response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+		      
+		      // parse the received body data
+		      var decodedBody = querystring.parse(fullBody);
+
+		      content = decodedBody.content;
+
+		      var newPath = genString();
+		      var url = 'http://portkeyapp.herokuapp.com//' + newPath;
+		      linkTable[newPath] = content;
+
+		      console.log('API REQUEST MADE');
+		      console.log(decodeURIComponent(content));
+
+
+		      // output the decoded data to the HTTP response          
+		      response.write('<html><head><title>Post data</title></head><body><a href="');
+		      response.write(url);
+		      response.write('">');
+		      response.write(url);
+		      response.write('</a></body></html>');
+		      
+		      response.end();
+		    });
+		}else{
+			response.writeHead(404, {'Content-Type': 'text/plain'});
+    		response.end("Page Could Not Be Found"); 
+		}
 	}
 
 });
@@ -36,6 +111,7 @@ server.listen( process.env.PORT  || 8080);
 // Intialize Now.js
 var everyone = nowjs.initialize(server);
 
+/*
 function project(name, link, image){
 	this.name = name;
 	this.link = link;
@@ -53,16 +129,20 @@ function queue(){
 }
 
 var projects = new queue();
+*/
 
 // Connect a user
 nowjs.on('connect', function(){
 	this.now.room = 'main';
 	nowjs.getGroup(this.now.room).addUser(this.user.clientId);
 
+	this.now.recieveContent(content);
+
+	/*
 	var list = projects.list
 	for(var i=0; i<list.length; i++){
 		this.now.recieveProject(list[i]);
-	}
+	}*/
 });
 
 // Disconnect a user
@@ -70,11 +150,12 @@ nowjs.on('disconnect', function(){
 
 });
 
+/*
 everyone.now.pushProject = function(projectObject){
 	nowjs.getGroup(this.now.room).now.recieveProject(projectObject);
 	projects.enq(projectObject);
 	console.log('added project '+ projectObject.name);
-}
+}*/
 
 
 
